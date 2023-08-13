81808752ca8613e083e9b4001dd93ef213132f5d
Jerry Abadi <abadijerry44@gmail.com>
2023-08-13 17:59:37 +0100

    Added New Files

---
+++ nav.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.btn--menu');
+
+  menuBtn.addEventListener('click', () => {
+  const isExpanded = JSON.parse(menuBtn.getAttribute('aria-expanded'));
+  menuBtn.setAttribute('aria-expanded', !isExpanded);
+})
+
